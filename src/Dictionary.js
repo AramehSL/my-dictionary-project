ae983e91746d372346a1d187572f51641f50a9ca
Arameh Soltani <aramehsl@gmail.com>
2022-06-06 00:17:41 -0400

    developed the axios search

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
+import Results from "./Results";
 import axios from "axios";
 
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -34,6 +36,7 @@ export default function Dictionary() {
         />
         <input type="submit" value="Search" />
       </form>
+      <Results results={results} />
     </div>
   );
 }
